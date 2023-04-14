9cf00da3e360f71fc354e968b797ee21fbfbfbb1
Max Timkovich <max@timkovi.ch>
2023-04-14 02:21:22 -0700

    typo, lower tv lights

--- weather.py
+++ weather.py
@@ -6,8 +6,12 @@ api_url = 'https://api.weather.gov/gridpoints/SEW/125,70/forecast'
 def is_cloudy():
     data = requests.get(api_url).json()
     weather = data['properties']['periods'][0]
+    # print(weather)
     forecast = weather['detailedForecast'].lower()
 
-    # print(forecast)
-    # print('cloudy' in forecast)
-    return 'cloudy' in forecast)
+    print(forecast)
+    print('cloudy' in forecast)
+    return 'cloudy' in forecast
+
+if __name__ == '__main__':
+    is_cloudy()
